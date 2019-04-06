bc55ace2c03b01e3ca34ec8b3f192aa67bb0d42b
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2019-04-06 16:28:27 +0200

    new approach, not yet working, backup

---
+++ spec/colorized-fading-tabs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ColorizedFadingTabsView from '../lib/colorized-fading-tabs-view';
+
+describe('ColorizedFadingTabsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
